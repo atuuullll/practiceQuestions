b16b1447935abdb607122b0293d68bb4fdcaeecb
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-01 16:37:13 +0000

    This file is for array

--- PracticeQuestions/nestedarray.js
+++ PracticeQuestions/nestedarray.js
@@ -1,3 +1,9 @@
 let Heroes = [["Superman", "Batman", "Wonder Woman"],
             ["Spiderman", "Ironman", "Thor", "Hulk"]];
 console.log(Heroes[1][3]);
+
+console.log(Heroes.length);
+let Newheroes01 = Heroes.pop();
+let Newheroes02 = Heroes.push(["Flash"]);
+console.log(Newheroes01);
+console.log(Newheroes02);
