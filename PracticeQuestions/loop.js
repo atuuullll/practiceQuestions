3bde0d07c40fcda238483acbfefe8cd4f8773448
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-02 17:55:55 +0000

    Add loop questions.

--- PracticeQuestions/loop.js
+++ PracticeQuestions/loop.js
@@ -1,1 +1,10 @@
-loop;
+let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
+if(car.length > 3){
+    console.log("Array has more than 3 elements");
+}
+console.log("Car length: ", car.length);
+let size = car.length;
+while(size >= 0){
+    console.log(car[size]);
+    size--;
+}
