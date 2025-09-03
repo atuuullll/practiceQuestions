3ee90c9bc0f4c7dba265a72c6bda9ba6fcdba5eb
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-03 17:12:48 +0000

    For loop

--- PracticeQuestions/loop.js
+++ PracticeQuestions/loop.js
@@ -1,10 +1,17 @@
+//While loop 
+// let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
+// if(car.length > 3){
+//     console.log("Array has more than 3 elements");
+// }
+// console.log("Car length: ", car.length);
+// let size = car.length;
+// while(size >= 0){
+//     console.log(car[size]);
+//     size--;
+// }
+
+
 let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
-if(car.length > 3){
-    console.log("Array has more than 3 elements");
-}
-console.log("Car length: ", car.length);
-let size = car.length;
-while(size >= 0){
-    console.log(car[size]);
-    size--;
+for(let i=0;i<car.length;i++){
+    console.log(car[i]);
 }
