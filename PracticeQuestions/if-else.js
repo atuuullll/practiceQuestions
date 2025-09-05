60d6456ceeb67f5ff70a783cc5f3dbe3c8b1c741
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-05 16:53:16 +0000

    This is an if-else code

---
+++ PracticeQuestions/if-else.js
@@ -0,0 +1,20 @@
+// let car = ["BMW", "Volvo", "Saab", "Ford"];
+// if (car[0] == "BMW") {
+//   console.log("This is BMW");
+// } else {
+//   console.log("This is not BMW");
+// }
+
+// let age = 20;
+// if (age < 18) {
+//   console.log("You are a minor");
+// } else {
+//   console.log("You are an adult");
+// }
+
+let num = 10;
+if (num % 2 == 0) {
+  console.log("The number is even");
+} else {
+  console.log("The number is odd");
+}
