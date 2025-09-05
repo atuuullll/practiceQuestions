1e0ed083c92be96e4806516ce0724b9f83a7171e
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-05 16:47:42 +0000

    It is a while loop question

--- PracticeQuestions/loop.js
+++ PracticeQuestions/loop.js
@@ -1,4 +1,12 @@
-//While loop 
+//While loop
+let num=5;
+while(num<=10){
+    console.log(num);
+    num++;
+}
+
+
+
 // let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
 // if(car.length > 3){
 //     console.log("Array has more than 3 elements");
@@ -11,7 +19,7 @@
 // }
 
 
-let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
-for(let i=0;i<car.length;i++){
-    console.log(car[i]);
-}
+// let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
+// for(let i=0;i<car.length;i++){
+//     console.log(car[i]);
+// }
