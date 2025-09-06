957bf5204a4a32cd8609bde832bbcfe37520d2fb
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-06 18:19:19 +0000

    While loop code

--- PracticeQuestions/whileloop.js
+++ PracticeQuestions/whileloop.js
@@ -1,5 +1,12 @@
-let num=5;
-while(num<=10){
-    console.log(num);
-    num++;
+// let num=5;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
+let i=0;
+while(i<car.length){
+    console.log(car[i]);
+    i++;
 }
