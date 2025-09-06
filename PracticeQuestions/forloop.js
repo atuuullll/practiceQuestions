41b0bf25f4c9253f6cd8eeebcf601d3d1a55b129
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-06 18:12:10 +0000

    Even number

--- PracticeQuestions/forloop.js
+++ PracticeQuestions/forloop.js
@@ -1,4 +1,11 @@
-let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
-for(let i=0;i<car.length;i++){
-    console.log(car[i]);
+// let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
+// for(let i=0;i<car.length;i++){
+//     console.log(car[i]);
+// }
+
+let num = [1,2,3,4,5,6,7,8,9];
+for(let i=0;i<num.length;i++){
+    if(num[i]%2==0){
+        console.log(num[i]);
+    }
 }
