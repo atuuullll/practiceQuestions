011116a81528ea36cad10abe26badbccb4d8c253
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-07 18:23:38 +0000

    new code but not pattern code

---
+++ PracticeQuestions/pattern.js
@@ -0,0 +1,13 @@
+let a=5;
+let b=10;
+let c=a+b;
+console.log(c);
+
+let str="hello";
+console.log(str);
+
+let arr=[1,2,3,4,5];
+console.log(arr);
+
+let obj={name:"John", age:30};
+console.log(obj);
--- PracticeQuestions/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-let car = ["AUDI" , "BMW" , "XUV" , "TATA"];
-// let i=0;
-// while(i<car.length){
-//     console.log(car[i]);
-//     i++;
-// }
