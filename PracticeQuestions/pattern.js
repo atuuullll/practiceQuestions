972e3e39e7c612b71a69d5170ea837d418e9a1c8
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-08 13:44:32 +0000

    Pattern codes

--- PracticeQuestions/pattern.js
+++ PracticeQuestions/pattern.js
@@ -11,3 +11,35 @@ console.log(arr);
 
 let obj={name:"John", age:30};
 console.log(obj);
+
+
+function greet(name){
+    return "Hello, " + name;
+}
+console.log(greet("Alice"));
+
+let isTrue=true;
+console.log(isTrue);
+
+let num=15;
+if(num%2===0){
+    console.log(num + " is even");
+}else{
+    console.log(num + " is odd");
+}
+
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+
+let sum=0;
+for(let i=1; i<=100; i++){
+    sum+=i;
+}
+console.log("Sum of first 100 natural numbers: " + sum);
+
+let factorial=1;
+for(let i=1; i<=5; i++){
+    factorial*=i;
+}
+console.log("Factorial of 5: " + factorial);
