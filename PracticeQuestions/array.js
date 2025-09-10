79a83a58c06ad618bbd9d817558658b9af1f5b1a
Atul Kumar Rawat <118012606+atuuullll@users.noreply.github.com>
2025-09-10 18:06:43 +0000

    Array codes

--- PracticeQuestions/array.js
+++ PracticeQuestions/array.js
@@ -1,2 +1,11 @@
 let array = [1, 2, 3, 4, 5];
 console.log(array);
+
+let newArray = array.map(x => x * 2);
+console.log(newArray);
+
+let filteredArray = newArray.filter(x => x > 5);
+console.log(filteredArray);
+
+let sum = filteredArray.reduce((acc, val) => acc + val, 0);
+console.log(sum);
